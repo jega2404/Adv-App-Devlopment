221a88600372086046dd97fbd0f22608a40541fe
JEGAJITH <114394648+jega2404@users.noreply.github.com>
2024-03-05 15:21:48 +0530

    Add files via upload

---
+++ Backend/Day5/src/main/java/com/example/edu_aid/Repository/UserDetailsRespository.java
@@ -0,0 +1,14 @@
+package com.example.edu_aid.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.edu_aid.Model.UserModel;
+
+// import firstsample.demo.Model.UserModel;
+
+public interface  UserDetailsRespository extends JpaRepository<UserModel, Long> {
+
+    @Query(value = "SELECT * FROM user_details WHERE email=:user_name",nativeQuery = true)
+    UserModel findByUsername(String user_name);
+    
+}
